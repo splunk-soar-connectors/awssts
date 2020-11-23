48f461100fc6bddf0756eb277df4153952a79073
Tony Cihak <tcihak@splunk.com>
2020-11-23 10:09:12 -0800

    PAPP-11413 initial code commit

---
+++ awssts_connector.py
@@ -0,0 +1,416 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# -----------------------------------------
+# Phantom sample App Connector python file
+# -----------------------------------------
+
+# Python 3 Compatibility imports
+from __future__ import print_function, unicode_literals
+
+# Phantom App imports
+import phantom.app as phantom
+from phantom.base_connector import BaseConnector
+from phantom.action_result import ActionResult
+
+# Usage of the consts file is recommended
+from awssts_consts import *
+from boto3 import client
+from datetime import datetime
+from botocore.config import Config
+import requests
+import json
+from bs4 import BeautifulSoup
+import six
+
+
+class RetVal(tuple):
+
+    def __new__(cls, val1, val2=None):
+        return tuple.__new__(RetVal, (val1, val2))
+
+
+class AwsSecureTokenServiceConnector(BaseConnector):
+
+    def __init__(self):
+
+        # Call the BaseConnectors init first
+        super(AwsSecureTokenServiceConnector, self).__init__()
+
+        self._state = None
+        self._region = None
+        self._access_key = None
+        self._secret_key = None
+        self._token = None
+        self._proxy = None
+
+    def initialize(self):
+        self._state = self.load_state()
+
+        config = self.get_config()
+
+        self._region = STS_REGION_DICT.get(config['region'])
+        if not self._region:
+            return self.set_status(phantom.APP_ERROR, "Specified region is not valid")
+
+        self._proxy = {}
+        env_vars = config.get('_reserve_environment_variables', {})
+        if 'HTTP_PROXY' in env_vars:
+            self._proxy['http'] = env_vars['HTTP_PROXY']['value']
+        if 'HTTPS_PROXY' in env_vars:
+            self._proxy['https'] = env_vars['HTTPS_PROXY']['value']
+
+        # Check for ec2 role creds, otherwise use the ones in the config
+        credentials = None
+        if config.get('use_role'):
+            credentials = self._handle_get_ec2_role()
+            if not credentials:
+                return self.set_status(phantom.APP_ERROR, "Failure to get ec2 role credentials")
+            else:
+                self._access_key = credentials._access_key
+                self._secret_key = credentials._secret_key
+                self._token = credentials._token
+
+        else:
+            self._access_key = config.get(STS_JSON_ACCESS_KEY)
+            self._secret_key = config.get(STS_JSON_SECRET_KEY)
+
+        return phantom.APP_SUCCESS
+
+    def finalize(self):
+        self.save_state(self._state)
+
+        return phantom.APP_SUCCESS
+
+    def _create_client(self, action_result, service='sts'):
+
+        boto_config = None
+        if self._proxy:
+            boto_config = Config(proxies=self._proxy)
+
+        try:
+            if self._access_key and self._secret_key:
+                self.debug_print("Creating boto3 client with API keys")
+
+                self._client = client(
+                    service,
+                    region_name=self._region,
+                    aws_access_key_id=self._access_key,
+                    aws_secret_access_key=self._secret_key,
+                    config=boto_config
+                )
+
+            else:
+                self.debug_print("Creating boto3 client without API keys")
+
+                self._client = client(
+                    'sts',
+                    region_name=self._region,
+                    config=boto_config
+                )
+        except Exception as e:
+            return action_result.set_status(phantom.APP_ERROR, "Could not create boto3 client: {0}".format(e))
+
+        return phantom.APP_SUCCESS
+
+    def _sanitize_dates(self, cur_obj):
+
+        try:
+            json.dumps(cur_obj)
+            return cur_obj
+        except:
+            pass
+
+        if isinstance(cur_obj, dict):
+            new_dict = {}
+            for k, v in six.iteritems(cur_obj):
+                new_dict[k] = self._sanitize_dates(v)
+            return new_dict
+
+        if isinstance(cur_obj, list):
+            new_list = []
+            for v in cur_obj:
+                new_list.append(self._sanitize_dates(v))
+            return new_list
+
+        if isinstance(cur_obj, datetime):
+            return cur_obj.strftime("%Y-%m-%d %H:%M:%S")
+
+        return cur_obj
+
+    def _process_empty_response(self, response, action_result):
+        if response.status_code == 200:
+            return RetVal(phantom.APP_SUCCESS, {})
+
+        return RetVal(
+            action_result.set_status(
+                phantom.APP_ERROR, "Empty response and no information in the header"
+            ), None
+        )
+
+    def _process_html_response(self, response, action_result):
+        # An html response, treat it like an error
+        status_code = response.status_code
+
+        try:
+            soup = BeautifulSoup(response.text, "html.parser")
+            error_text = soup.text
+            split_lines = error_text.split('\n')
+            split_lines = [x.strip() for x in split_lines if x.strip()]
+            error_text = '\n'.join(split_lines)
+        except:
+            error_text = "Cannot parse error details"
+
+        message = "Status Code: {0}. Data from server:\n{1}\n".format(status_code, error_text)
+
+        message = message.replace(u'{', '{{').replace(u'}', '}}')
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _process_json_response(self, r, action_result):
+        # Try a json parse
+        try:
+            resp_json = r.json()
+        except Exception as e:
+            return RetVal(
+                action_result.set_status(
+                    phantom.APP_ERROR, "Unable to parse JSON response. Error: {0}".format(str(e))
+                ), None
+            )
+
+        # Please specify the status codes here
+        if 200 <= r.status_code < 399:
+            return RetVal(phantom.APP_SUCCESS, resp_json)
+
+        # You should process the error returned in the json
+        message = "Error from server. Status Code: {0} Data from server: {1}".format(
+            r.status_code,
+            r.text.replace(u'{', '{{').replace(u'}', '}}')
+        )
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _process_response(self, r, action_result):
+        # store the r_text in debug data, it will get dumped in the logs if the action fails
+        if hasattr(action_result, 'add_debug_data'):
+            action_result.add_debug_data({'r_status_code': r.status_code})
+            action_result.add_debug_data({'r_text': r.text})
+            action_result.add_debug_data({'r_headers': r.headers})
+
+        # Process each 'Content-Type' of response separately
+
+        # Process a json response
+        if 'json' in r.headers.get('Content-Type', ''):
+            return self._process_json_response(r, action_result)
+
+        # Process an HTML response, Do this no matter what the api talks.
+        # There is a high chance of a PROXY in between phantom and the rest of
+        # world, in case of errors, PROXY's return HTML, this function parses
+        # the error and adds it to the action_result.
+        if 'html' in r.headers.get('Content-Type', ''):
+            return self._process_html_response(r, action_result)
+
+        # it's not content-type that is to be parsed, handle an empty response
+        if not r.text:
+            return self._process_empty_response(r, action_result)
+
+        # everything else is actually an error at this point
+        message = "Can't process response from server. Status Code: {0} Data from server: {1}".format(
+            r.status_code,
+            r.text.replace('{', '{{').replace('}', '}}')
+        )
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _make_rest_call(self, endpoint, action_result, method="get", **kwargs):
+        # **kwargs can be any additional parameters that requests.request accepts
+
+        config = self.get_config()
+
+        resp_json = None
+
+        try:
+            request_func = getattr(requests, method)
+        except AttributeError:
+            return RetVal(
+                action_result.set_status(phantom.APP_ERROR, "Invalid method: {0}".format(method)),
+                resp_json
+            )
+
+        # Create a URL to connect to
+        url = self._base_url + endpoint
+
+        try:
+            r = request_func(
+                url,
+                # auth=(username, password),  # basic authentication
+                verify=config.get('verify_server_cert', False),
+                **kwargs
+            )
+        except Exception as e:
+            return RetVal(
+                action_result.set_status(
+                    phantom.APP_ERROR, "Error Connecting to server. Details: {0}".format(str(e))
+                ), resp_json
+            )
+
+        return self._process_response(r, action_result)
+
+    def _make_boto_call(self, action_result, method, **kwargs):
+
+        try:
+            boto_func = getattr(self._client, method)
+        except AttributeError:
+            return RetVal(action_result.set_status(phantom.APP_ERROR, "Invalid method: {0}".format(method)), None)
+
+        try:
+            resp_json = boto_func(**kwargs)
+        except Exception as e:
+            return RetVal(action_result.set_status(phantom.APP_ERROR, "boto3 call to STS failed", e), None)
+
+        # return phantom.APP_SUCCESS, self._sanitize_dates(resp_json)
+        return phantom.APP_SUCCESS, self._sanitize_dates(resp_json)
+
+    def _handle_test_connectivity(self, param):
+
+        self.save_progress("Querying STS to check credentials")
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        if not self._create_client(action_result):
+            return action_result.get_status()
+
+        # ret_val, resp_json = self._make_boto_call(action_result, 'get_access_key_info',
+        #                                          AccessKeyId=self._access_key)
+        ret_val, resp_json = self._make_boto_call(action_result, 'get_caller_identity')
+
+        if phantom.is_fail(ret_val):
+            self.save_progress("Test Connectivity Failed.")
+            return ret_val
+
+        self.save_progress("Test Connectivity Passed")
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def _handle_assume_role(self, param):
+
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        role_session_name = param.get('role_session_name',
+                                      'Request_from_Phantom_by_user_X_for_Phantom_Event_Y')
+        external_id = param.get('external_id', None)
+        role_arn = param.get('role_arn', None)
+
+        # create client
+        if phantom.is_fail(self._create_client(action_result, service='sts')):
+            return action_result.get_status()
+
+        # make boto3 call
+        if external_id:
+            ret_val, resp_json = self._make_boto_call(action_result, 'assume_role',
+                                                      RoleSessionName=role_session_name,
+                                                      RoleArn=role_arn,
+                                                      ExternalId=external_id)
+        else:
+            ret_val, resp_json = self._make_boto_call(action_result, 'assume_role',
+                                                      RoleSessionName=role_session_name,
+                                                      RoleArn=role_arn)
+        if phantom.is_fail(ret_val):
+            pass
+
+        action_result.add_data(resp_json)
+
+        # summary = action_result.set_summary({})
+        # summary['Credentials'] = resp_json.get('Credentials', 0)
+
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def handle_action(self, param):
+        ret_val = phantom.APP_SUCCESS
+
+        # Get the action that we are supposed to execute for this App Run
+        action_id = self.get_action_identifier()
+
+        self.debug_print("action_id", self.get_action_identifier())
+
+        if action_id == 'test_connectivity':
+            ret_val = self._handle_test_connectivity(param)
+
+        elif action_id == 'assume_role':
+            ret_val = self._handle_assume_role(param)
+
+        return ret_val
+
+    def _handle_get_ec2_role(self):
+
+        session = boto3.Session(region_name=self._region)
+        credentials = session.get_credentials()
+        return credentials
+
+
+def main():
+    import pudb
+    import argparse
+
+    pudb.set_trace()
+
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument('input_test_json', help='Input Test JSON file')
+    argparser.add_argument('-u', '--username', help='username', required=False)
+    argparser.add_argument('-p', '--password', help='password', required=False)
+
+    args = argparser.parse_args()
+    session_id = None
+
+    username = args.username
+    password = args.password
+
+    if username is not None and password is None:
+
+        # User specified a username but not a password, so ask
+        import getpass
+        password = getpass.getpass("Password: ")
+
+    if username and password:
+        try:
+            login_url = AwsSecureTokenServiceConnector._get_phantom_base_url() + '/login'
+
+            print("Accessing the Login page")
+            r = requests.get(login_url, verify=False)
+            csrftoken = r.cookies['csrftoken']
+
+            data = dict()
+            data['username'] = username
+            data['password'] = password
+            data['csrfmiddlewaretoken'] = csrftoken
+
+            headers = dict()
+            headers['Cookie'] = 'csrftoken=' + csrftoken
+            headers['Referer'] = login_url
+
+            print("Logging into Platform to get the session id")
+            r2 = requests.post(login_url, verify=False, data=data, headers=headers)
+            session_id = r2.cookies['sessionid']
+        except Exception as e:
+            print("Unable to get session id from the platform. Error: " + str(e))
+            exit(1)
+
+    with open(args.input_test_json) as f:
+        in_json = f.read()
+        in_json = json.loads(in_json)
+        print(json.dumps(in_json, indent=4))
+
+        connector = AwsSecureTokenServiceConnector()
+        connector.print_progress_message = True
+
+        if session_id is not None:
+            in_json['user_session_token'] = session_id
+            connector._set_csrf_info(csrftoken, headers['Referer'])
+
+        ret_val = connector._handle_action(json.dumps(in_json), None)
+        print(json.dumps(json.loads(ret_val), indent=4))
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
---
+++ awssts_consts.py
@@ -0,0 +1,32 @@
+# --
+# File: awssts_consts.py
+#
+# Copyright (c) 2018-2020 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+#
+# --
+
+STS_JSON_ACCESS_KEY = "access_key"
+STS_JSON_SECRET_KEY = "secret_key"
+
+STS_REGION_DICT = {
+        "US East (Ohio)": "us-east-1",
+        "US East (N. Virginia)": "us-east-2",
+        "US West (N. California)": "us-west-1",
+        "US West (Oregon)": "us-west-2",
+        "Canada (Central)": "ca-central-1",
+        "Asia Pacific (Mumbai)": "ap-south-1",
+        "Asia Pacific (Tokyo)": "ap-northeast-1",
+        "Asia Pacific (Seoul)": "ap-northeast-2",
+        "Asia Pacific (Singapore)": "ap-southeast-1",
+        "Asia Pacific (Sydney)": "ap-southeast-2",
+        "China (Ningxia)": "cn-northwest-1",
+        "EU (Frankfurt)": "eu-central-1",
+        "EU (Ireland)": "eu-west-1",
+        "EU (London)": "eu-west-2",
+        "South Americia (Sao Paulo)": "sa-east-1",
+        "US GovCloud East": "us-gov-east-1",
+        "US GovCloud West": "us-gov-west-1",
+    }
